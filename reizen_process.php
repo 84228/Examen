a57c20c27165931d8d38809c18533a04964e8c1e
David <74043362+84228@users.noreply.github.com>
2022-05-17 17:09:56 +0200

    Update reizen_process.php

--- reizen_process.php
+++ reizen_process.php
@@ -1,44 +1,42 @@
 <?php 
 session_start();
-$reisID   = $_SESSION['reisID'];
+$userID = $_SESSION['userID'];
 
 require_once 'config.php'; 
 
-$id = 0;
+$opmerkingen = '';
 $update = false;
-$name = ''; 
-$location = '';
 
-//Voeg een inschrijving toe
+//Voeg een inschrijving toe / inschrijvingen +1
 if (isset($_POST['save'])) {
-    $id = $_POST['id'];
-    $naam = $_POST['naam'];
-    $student = $_POST['studentennummer'];
     $ID = $_POST['ID'];
-    $opmerkingen = $_POST['opmerkingen']
-    $counter = $_POST['counter'];
+    $opmerkingen = $_POST['opmerkingen'];
 
-    $mysqli->query("INSERT INTO inschrijvingen (id, naam, studentennummer, reisID, opmerkingen) VALUES('$id', '$naam', '$student', '$ID', '$opmerkingen')") or 
-        die($mysqli->error);
-    $mysqli->query("UPDATE INTO reizen (counter) VALUES('$counter') WHERE reisID is $ID") or 
-        die($mysqli->error);
+    
+    $mysqli->query("UPDATE Users SET opmerkingen='$opmerkingen', reisID='$ID' WHERE id=$userID") or 
+        die($mysqli->error());   
+    
+    $mysqli->query("UPDATE reizen SET counter = counter + 1 WHERE reisID = $ID") or 
+        die($mysqli->error());  
 
     $_SESSION['message'] = "Record has been saved!";
     $_SESSION['msg_type'] = "success";
 
-    header("location: index.php");
+    header("location: overzicht.php");
 }
 
-//Delete een inschrijving
+//Delete een gebruiker / inschrijvingen -1 werkt nog niet met de counter 
 if (isset($_GET['delete'])) {
-    $id = $_GET['delete'];
-    $mysqli->query("DELETE FROM inschrijvingen WHERE id=$id") or die($mysqli->error());
-    $mysqli->query("UPDATE INTO reizen (counter) VALUES('$counter2' WHERE reisID is $ID)") or 
-        die($mysqli->error);
+    $userID = $_GET['delete'];
+    $reisID = $_GET['ID'];
+    
+    $mysqli->query("UPDATE Users SET reisID = 0 WHERE id=$userID") or die($mysqli->error());
+    $mysqli->query("UPDATE reizen SET counter = counter - 1 WHERE reisID = $ID") or die($mysqli->error());   
+
     $_SESSION['message'] = "Record has been deleted!";
     $_SESSION['msg_type'] = "danger";
 
-    header("location: index.php");
+    header("location: overzicht.php");
 }
 
 //Laat een gebruiker / record zien wanneer je op edit klikt
@@ -46,28 +44,27 @@ if (isset($_GET['delete'])) {
 if (isset($_GET['edit'])) {
     $id = $_GET['edit']; 
     $update = true; 
-    $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error());
+    $result = $mysqli->query("SELECT * FROM Users WHERE id=$userID") or die($mysqli->error());
     
     if (count($result)==1) {
         $row = $result->fetch_array();
-        $name = $row['name'];
-        $location = $row['location'];
+        $opmerkingen = $row['opmerkingen'];
     }
 }
 
 
-//Update een gebruiker / Record
+//Update de counter zet hier de plus 1 counter neer
 if (isset($_POST['update'])) { 
     $id = $_POST['id'];
-    $name = $_POST['name']; 
-    $location = $_POST['location'];
+    $opmerkingen = $_POST['opmerkingen']; 
+
 
     $mysqli->query("UPDATE data SET name='$name', location='$location' WHERE id=$id") or die($mysqli->error()); 
 
     $_SESSION['message'] = "Record has been updated!";
     $_SESSION['msg_type'] = "warning";
 
-    header('location: index.php');
+    header('location: overzicht.php');
 }
 
 
