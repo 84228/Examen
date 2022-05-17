8575a7620689f2957cccf7ce311ca25902a67e23
David <74043362+84228@users.noreply.github.com>
2022-05-17 17:08:43 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -68,6 +68,33 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
             </div>
         </nav>
     <?php
+    } if (strpos($url,'reizen') !== false) {
+        ?>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php">Ga Lekker Reizen</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="overzicht.php">Overzicht</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="reizen.php">Reizen</a>
+                            </li>
+                            <li class="nav-item">
+                                <?php echo $login; ?>
+                            </li>        
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        <?php 
     } if (strpos($url,'login') !== false) {
     ?>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -84,7 +111,7 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
                         <li class="nav-item">
                             <a class="nav-link" href="overzicht.php">Overzicht</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item active">
                             <?php echo $login; ?>
                         </li>        
                     </ul>
@@ -108,7 +135,7 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
                     <li class="nav-item">
                         <a class="nav-link" href="overzicht.php">Overzicht</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item active">
                         <?php echo $login; ?>
                     </li>          
                 </ul>
